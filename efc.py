11bfc78446ae7616ab3096473e8cfb9cbac8079e
tringcooler <tringcooler@users.noreply.github.com>
2025-05-22 03:19:57 +0800

    Update efc.py

--- efc.py
+++ efc.py
@@ -89,17 +89,46 @@ def _feb_unit_dec(buf, blen, bla):
     val, buf, blen, bla = _feb_adic_dec(buf, blen, bla)
     return cmd, val, buf, blen, bla
 
+def _feb_enc(seq, buf, blen):
+    for val in seq:
+        if isinstance(val, list):
+            #TODO
+            pass
+        else:
+            buf, blen = _feb_unit_enc(1, val, buf, blen)
+    return buf, blen
+
+def _feb_dec(buf, blen, bla):
+    seq = []
+    while blen >= 0:
+        cmd, val, buf, blen, bla = _feb_unit_dec(buf, blen, bla)
+        if cmd == 1:
+            seq.append(val)
+        else:
+            #TODO
+            pass
+    return seq
+
 if __name__ == '__main__':
     import pdb
     from hexdump import hexdump as hd
     from pprint import pprint
     ppr = lambda *a, **ka: pprint(*a, **ka, sort_dicts = False)
 
-    def main(n):
+    def test1(n):
         for i in range(1, n+1):
             #cd, ln = _feb_adic_enc(i, 0, 0)
             cd, ln = _feb_unit_enc(1, i, 0, 0)
             cmd, dv, _, blen, _ = _feb_unit_dec(*_buf_pop(cd, ln))
             print(i, bin(cd), ln, '->', cmd, dv)
             assert cmd == 1 and dv == i and blen == -1
-    main(20)
+    #test1(20)
+
+    def test2(n):
+        seq = [i for i in range(1, n+1)]
+        buf, blen = _feb_enc(seq, 0, 0)
+        dseq = _feb_dec(*_buf_pop(buf, blen))
+        print(buf, blen, seq, dseq)
+        assert seq == dseq
+        return buf
+    buf = test2(20)
